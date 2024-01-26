a765d4fcbb643838ced2f213cc490632b8f2929d
ApasaJB <jb.apasa@gmail.com>
2024-01-26 18:58:55 +0100

    init: create the app

---
+++ src/main/java/net/apasajb/activbox/ActivboxApplication.java
@@ -0,0 +1,14 @@
+package net.apasajb.activbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ActivboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/apasajb/activbox/ActivboxApplicationTests.java
@@ -0,0 +1,13 @@
+package net.apasajb.activbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
