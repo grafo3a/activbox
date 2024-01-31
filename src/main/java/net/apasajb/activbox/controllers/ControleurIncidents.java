4e52576d0f2161f777e3177401a0e15bc186d71d
ApasaJB <jb.apasa@gmail.com>
2024-01-31 21:39:22 +0100

    feat: add controllers for incidents, changes & problems

---
+++ src/main/java/net/apasajb/activbox/controllers/ControleurIncidents.java
@@ -0,0 +1,12 @@
+package net.apasajb.activbox.controllers;
+
+import org.springframework.stereotype.Controller;
+
+
+/**
+ * Controleur pour les incidents
+ */
+@Controller
+public class ControleurIncidents {
+
+}
---
+++ src/main/java/net/apasajb/activbox/controllers/ControleurProblemes.java
@@ -0,0 +1,12 @@
+package net.apasajb.activbox.controllers;
+
+import org.springframework.stereotype.Controller;
+
+
+/**
+ * Controleur pour les problemes
+ */
+@Controller
+public class ControleurProblemes {
+
+}
