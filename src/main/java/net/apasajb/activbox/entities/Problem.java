2ed3035b7a6ad61b70112c6b79c846ceaf034024
ApasaJB <jb.apasa@gmail.com>
2024-02-26 22:10:56 +0100

    feat: add the entity class problem.java

---
+++ src/main/java/net/apasajb/activbox/entities/Problem.java
@@ -0,0 +1,38 @@
+package net.apasajb.activbox.entities;
+
+import org.springframework.stereotype.Component;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+/**
+ * Entité qui représente un problème en BDD
+ * @author ApasaJB
+ */
+@Component
+@Entity
+@Table(name="problemes")
+public class Problem {
+	
+	@Id
+	@Column(name="id_probleme")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer idProbleme;
+	
+	@Column(name="numero_probleme", length=20, nullable=false, unique=true)
+	private String numeroProbleme;
+	
+	
+	// Constructeurs par defaut et parametrique
+	
+	
+	// To string
+	
+	
+	//======================================= GETTERS & SETTERS //
+	//===========================================================//
+}
