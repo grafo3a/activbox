bc2c1c207b9d550e2ba11b2362325ff9c8a120eb
ApasaJB <jb.apasa@gmail.com>
2024-02-26 21:58:47 +0100

    feat: add the entity class change.java

---
+++ src/main/java/net/apasajb/activbox/entities/Change.java
@@ -0,0 +1,38 @@
+package net.apasajb.activbox.entities;
+
+import org.springframework.stereotype.Component;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+/**
+ * Entité qui représente un changement en BDD
+ * @author ApasaJB
+ */
+@Component
+@Entity
+@Table(name="changements")
+public class Change {
+	
+	@Id
+	@Column(name="id_changement")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer idChangement;
+	
+	@Column(name="numero_changement", length=20, nullable=false, unique=true)
+	private String numeroChangement;
+	
+	
+	// Constructeurs par defaut et parametrique
+	
+	
+	// To string
+	
+	
+	//======================================= GETTERS & SETTERS //
+	//===========================================================//
+}
