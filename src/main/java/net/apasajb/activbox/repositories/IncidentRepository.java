41a29dda79b743bee807131bdb472cecac33a9d7
ApasaJB <jb.apasa@gmail.com>
2024-05-08 19:16:49 +0200

    feat: add the method findlisteincidentsetatouvert

--- src/main/java/net/apasajb/activbox/repositories/IncidentRepository.java
+++ src/main/java/net/apasajb/activbox/repositories/IncidentRepository.java
@@ -3,6 +3,7 @@ package net.apasajb.activbox.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import net.apasajb.activbox.entities.Incident;
 
@@ -27,4 +28,10 @@ public interface IncidentRepository extends JpaRepository<Incident, Integer> {
 	 * La recherche s'effectue sur la colonne du sujet.
 	 */
 	List<Incident> findByCol15SujetContaining(String motClef);
+	
+	/**
+	 * Recherche la liste des incidents actifs dans une equipe.
+	 */
+	@Query("FROM Incident WHERE col12EquipeEnCharge =?1 AND col11Etat != 'clos'")
+	List<Incident> findListeIncidentsEtatOuvert(String equipeEnCharge);
 }
