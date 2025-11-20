96b2eb02d2f74d526569f906a5d36a22578bd2ae
ApasaJB <jb.apasa@gmail.com>
2025-11-20 15:47:11 +0100

    feat: make I18nConfig.java implement WebMvcConfigurer

--- src/main/java/net/apasajb/activbox/config/I18nConfig.java
+++ src/main/java/net/apasajb/activbox/config/I18nConfig.java
@@ -5,47 +5,51 @@ import java.util.Locale;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 
 @Configuration
-public class I18nConfig {
-	
-	
-	@Bean
-	public MessageSource messageSource() {
-	    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-	    messageSource.setBasename("classpath:messages");
-	    messageSource.setDefaultEncoding("UTF-8");
-	    return messageSource;
-	}
-
-	
-	
-	
+public class I18nConfig implements WebMvcConfigurer {
 	
 	@Bean
 	public LocaleResolver localeResolver() {
-	    SessionLocaleResolver slr = new SessionLocaleResolver();
-	    slr.setDefaultLocale(Locale.ENGLISH);
-	    return slr;
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.ENGLISH);
+		return slr;
 	}
-	
-	
 	
 	@Bean
 	public LocaleChangeInterceptor localeChangeInterceptor() {
-	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-	    lci.setParamName("lang");
-	    return lci;
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
 	}
-
+	
+	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-	    registry.addInterceptor(localeChangeInterceptor());
+		registry.addInterceptor(localeChangeInterceptor());
 	}
-
+	
+	@Bean
+	@Primary
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		// Pour Thymeleaf (avec l'annotation Primary).
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+	
+	@Bean
+	public MessageSource javaMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		// Pour le code Java.
+		messageSource.setBasename("java");
+		return messageSource;
+	}
 }
